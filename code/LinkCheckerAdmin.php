8fcdae1f526f6d4fe63740268ee1b103d770b8c8
sharvey <sharvey@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-10-13 04:19:04 +0000

    removed comment
    git-svn-id: http://svn.silverstripe.com/open/modules/linkchecker/trunk@64135 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/LinkCheckerAdmin.php
+++ code/LinkCheckerAdmin.php
@@ -3,12 +3,6 @@
 /**
  * Link checker CMS interface.
  * 
- * This handles the user interface for running
- * instances of the link checker task
- * {@link LinkcheckTask}. For each run instance,
- * a table is generated with the broken link
- * results.
- * 
  * @package linkchecker
  */
 class LinkCheckerAdmin extends LeftAndMain {
