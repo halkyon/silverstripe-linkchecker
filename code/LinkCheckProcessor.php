b505ce70d0abaae41829427369bc0ac776256367
sharvey <sharvey@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-10-13 21:03:40 +0000

    Fixed the use of CURL for getting status code and headers
    git-svn-id: http://svn.silverstripe.com/open/modules/linkchecker/trunk@64150 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/LinkCheckProcessor.php
+++ code/LinkCheckProcessor.php
@@ -3,19 +3,6 @@
 class LinkCheckProcessor extends Object {
 
 	/**
-	 * This is a list of all the status
-	 * codes that are used for the link
-	 * checker.
-	 * 
-	 * @var array
-	 */
-	protected static $status_codes = array(
-		'404' => 'Not Found - Link doesn\'t exist',
-		'500' => 'Internel Server Error - Failed to show page because server had an error',
-		'200' => 'OK - Link is fine'
-	);
-	
-	/**
 	 * The agent name for this process, used
 	 * in the HTTP headers.
 	 *
@@ -46,6 +33,9 @@ class LinkCheckProcessor extends Object {
 	 * whenever this task is invoked.
 	 */
 	function process() {
+		$missing_links = 0;
+		$check_links = 0;
+		$good_links = 0;
 
 		// This could take a while to run, so set no time limit
 		set_time_limit(0);
@@ -53,45 +43,52 @@ class LinkCheckProcessor extends Object {
 		/* This is used for ob_flush to work */
 		if(ob_get_length() === false) ob_start();
 		
-		// Get the HTML for the URL
+		// Get the HTML from the URL
 		$html = $this->fetchHTML($this->url);
-		
+
+		// If no HTML returned, link doesn't exist, so show error
 		if(!$html) return "$this->url doesn't appear to exist.\r\n";
 
+		// Extract all the links from the HTML
 		$links = $this->extractLinks($html, $this->url);
+		
+		// HTML stored in memory is no longer required
 		unset($html);
 	
-		echo "Getting HTML from $this->url" . "\r\n";
+		echo "Getting HTML from {$this->url}\r\n";
 	
-		if(empty($links)) {
-			return "$this->url doesn't appear to have any links.\r\n";
-		} else {
-			$links         = array_values(array_unique($links));
-			$missing_links = 0;
-			$check_links   = 0;
-			$good_links    = 0;
-	
-			$linkCount = count($links);
+		if(empty($links)) return "$this->url doesn't appear to have any links.\r\n";
+		
+		// We only need unique links, so take any duplicates and discard them
+		$links = array_values(array_unique($links));
+		$linkCount = count($links);
+		
+		echo "Found {$linkCount} links on {$this->url}\r\n";
+
+		flush();
+		ob_flush();
+
+		// Each unique link needs to be checked to see what status code is returned
+		for($i = 0; isset($links[$i]) && !connection_aborted(); ++$i) {
+				
+			/* give server a break after checking each link */
+			usleep(500000);
+				
+			$headers = $this->fetchHeaders(html_entity_decode($links[$i]));
+
+			// If available, get the status code from the headers directly
+			if(!empty($headers['http_code'])) {
+				$status = $headers['http_code'];
+			} else {
+				// Conventional method is to extract it from the text
+				$status = $this->extractStatusCode($headers);
+			}
+				
+			var_dump($status);
 			
-			echo "Found {$linkCount} links on {$this->url}\r\n";
-	
+			// Flush the output buffer
 			flush();
 			ob_flush();
-	
-			for($i = 0; isset($links[$i]) && !connection_aborted(); ++$i) {
-					
-				/* give server a break after checking each url for ha"\r\n" a sec */
-				usleep(500000);
-					
-				$headers = $this->fetchHeaders(html_entity_decode($links[$i]));
-	
-				$status  = $this->fetchStatusCode($headers);
-					
-				var_dump($status);
-					
-				flush();
-				ob_flush();
-			}
 		}
 	}
 
@@ -179,22 +176,25 @@ class LinkCheckProcessor extends Object {
 	 */
 	function fetchHTML($url) {
 		$contents = '';
-	
+		
 		ini_set('user_agent', 'User-Agent: ' . self::$agent_name);
 		
 		if(($fp = fopen($url, 'r'))) {
 			for(;($data = fread($fp, 1024));) $contents .= $data;
 			fclose($fp);
 		} elseif(function_exists('curl_init')) {
-			$ch = curl_init( );
-	
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: ' . self::$agent_name);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	
+			$ch = curl_init($url);
+			
+			$options = array(
+				CURLOPT_HEADER => false,
+				CURLOPT_USERAGENT => self::$agent_name,
+				CURLOPT_RETURNTRANSFER => true
+			);
+			
+			curl_setopt_array($ch, $options);
+
 			$contents .= curl_exec($ch);
-	
+			
 			curl_close($ch);
 		} elseif(($url_info = parse_url($url))) {
 			if($url_info['scheme'] == 'https') {
@@ -202,24 +202,24 @@ class LinkCheckProcessor extends Object {
 			} else {
 				$fp = fsockopen($url_info['host'], 80, $errno, $errstr, 30);
 			}
-	
+			
 			if(!$fp) return false;
-	
+			
 			$out = 'HEAD ' . (isset($url_info['path']) ? $url_info['path'] : '/') .
 				(isset($url_info['query']) ? '?' . $url_info['query']: '') .
 				" HTTP/1.0\r\n";
-				
+			
 			$out .= 'Host: ' . $url_info['host'] . "\r\n";
 			$out .= 'User-Agent: ' . self::$agent_name . "\r\n";
 			$out .= "Connection: Close\r\n\r\n";
-	
+			
 			fwrite($fp, $out);
-
+			
 			for(;!feof($fp);) $contents .= fgets($fp, 128);
-	
+			
 			list($headers, $content) = explode("\r\n\r\n", $contents, 2);
 		}
-	
+		
 		return $contents;
 	}
 	
@@ -232,7 +232,25 @@ class LinkCheckProcessor extends Object {
 	function fetchHeaders($url) {
 		$headers = '';
 
-		if(($url_info = parse_url($url))) {
+		if(function_exists('curl_init')) {
+			$ch = curl_init($url);
+
+			$options = array(
+				CURLOPT_HEADER => false,
+				CURLOPT_NOBODY => true,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_USERAGENT => self::$agent_name,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => ''
+			);
+			
+			curl_setopt_array($ch, $options);
+			curl_exec($ch);
+			
+			$headers = curl_getinfo($ch);
+			
+			curl_close($ch);
+		} elseif(($url_info = parse_url($url))) {
 			if($url_info['scheme'] == 'https') {
 				$fp = fsockopen('ssl://' . $url_info['host'], 443, $errno, $errstr, 30);
 			} else {
@@ -263,12 +281,12 @@ class LinkCheckProcessor extends Object {
 	}
 	
 	/**
-	 * Get the status code from the headers.
+	 * Pull out the HTTP status code from the headers.
 	 *
 	 * @param array $headers List of headers
 	 * @return array
 	 */
-	function fetchStatusCode($headers) {
+	function extractStatusCode($headers) {
 		for($i = 0; isset($headers[$i]); $i++) {
 			// Checks if the header is the status header
 			if(preg_match("/HTTP\/[0-9A-Za-z +]/i", $headers[$i])) {
