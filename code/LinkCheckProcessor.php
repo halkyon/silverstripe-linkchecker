2fdfe28ca9e37a113022d6a086a8569b92c14cba
sharvey <sharvey@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-10-13 22:25:32 +0000

    Code tidy up and removed curl (we just use fopen, parse_url() which are PHP functions)
    git-svn-id: http://svn.silverstripe.com/open/modules/linkchecker/trunk@64158 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/LinkCheckProcessor.php
+++ code/LinkCheckProcessor.php
@@ -2,12 +2,12 @@
 
 /**
  * This class handles taking a URL, taking the HTML that
- * is returned from a URL - by using CURL, or parse_url()
- * and then extracting all the href values from <a> elements
- * in the HTML. Each of those href values is then checked
- * to see what the response code returned is - we do this
- * in a similar method to retrieving the HTML and headers,
- * by using CURL or parse_url().
+ * is returned from a URL - then extracting all the href
+ * values from <a> elements in the HTML.
+ * 
+ * Each of those href values is then checked to see what
+ * the response code returned is, e.g. "200" or "404" to
+ * determine what the status of the link is.
  * 
  * These href values are ignored:
  * 
@@ -95,13 +95,7 @@ class LinkCheckProcessor extends Object {
 				
 			$headers = $this->fetchHeaders(html_entity_decode($links[$i]));
 
-			// If available, get the status code from the headers directly
-			if(!empty($headers['http_code'])) {
-				$status = $headers['http_code'];
-			} else {
-				// Conventional method is to extract it from the header
-				$status = $this->extractStatusCode($headers);
-			}
+			$status = $this->extractStatusCode($headers);
 				
 			var_dump($status);
 			
@@ -122,80 +116,63 @@ class LinkCheckProcessor extends Object {
 	 * @return array List of all the links extracted from the HTML
 	 */
 	function extractLinks($html, $url) {
-	    $links    = array();
-	    $url_info = parse_url( $url );
+		$links = array();
+		$url_info = parse_url($url);
 	
-	    preg_match_all( "/<a[\s]+[^>]*?href[\s]?=[\s\"\']+(.*?)[\"\']+.*?>/", $html, $matches );
+		preg_match_all("/<a[\s]+[^>]*?href[\s]?=[\s\"\']+(.*?)[\"\']+.*?>/", $html, $matches);
 	
-	    if( empty( $url_info['path'] ) )
-	        $url_info['path'] = '/';
+		if(empty($url_info['path'])) $url_info['path'] = '/';
 	
-	    /* if there is a file at the end of the url get it */
-	    if( $url_info['path']{strlen( $url_info['path'] ) - 1} != '/' )
-	        $url_info['path'] = substr( $url_info['path'], 0, strrpos( $url_info['path'], '/' ) + 1 );
+		// If there is a file at the end of the URL, then get it
+		if($url_info['path']{strlen($url_info['path']) - 1} != '/') {
+			$url_info['path'] = substr( $url_info['path'], 0, strrpos($url_info['path'], '/') + 1);
+		}
+		
+		if(substr($url_info['host'], 0, 4) == 'www.') $host = substr($url_info['host'], 4) . '/';
+		else $host = $url_info['host'];
+
+		for($i = 0; isset($matches[1][$i]); $i++) {
+			if($matches[1][$i]{0} != '#' && !strpos($matches[1][$i], '@')) { // stop #top sort of links and remove emails
+				if(strpos($matches[1][$i], '#')) $matches[1][$i] = substr($matches[1][$i], 0, strpos($matches[1][$i], '#'));
 	
-	    if( substr( $url_info['host'], 0, 4 ) == 'www.' )
-	        $host = substr( $url_info['host'], 4 ) . '/';
-	    else
-	        $host = $url_info['host'];
-	
-	    for( $i=0; isset( $matches[1][$i] ); $i++ )
-	    {
-	        if( $matches[1][$i]{0} != '#' && ! strpos( $matches[1][$i], '@' ) ) // stop #top sort of links and remove emails
-	        {
-	
-	            if( strpos( $matches[1][$i], '#' ) )
-	                $matches[1][$i] = substr( $matches[1][$i], 0, strpos( $matches[1][$i], '#' ) );
-	
-	            if( $matches[1][$i]{0} == '/' ) // add host to any links
-	                $links[] = $url_info['scheme'] . '://' . $url_info['host'] . $matches[1][$i];
-	            elseif( $matches[1][$i]{0} == '.' )
-	            {
-	                $done    = true;
-	                $url     = $matches[1][$i];
-	                $cur_dir = explode( '/', $url_info['path'] );
-	                array_shift($cur_dir );
-	                array_pop($cur_dir );
-	
-	                for( $j=0; isset( $cur_dir[$j] ); $j++ )
-	                    $cur_dir[$j] = '/' . $cur_dir[$j];
-	
-	                for(;$done;)
-	                {
-	                    /*  if no more ./ or ../ then it's done */
-	                    if( $url{0} != '.' )
-	                    {
-	                        $links[] = $url_info['scheme'] . '://' . $url_info['host'] . implode( '', $cur_dir ) . '/' . $url;
-	                        $done    = false;
-	                    }
-	                    /* remove same dir as that is the default */
-	                    elseif( substr( $url, 0, 2 ) == './' )
-	                        $url = substr( $url, 2 );
-	                    else
-	                    {
-	                        $url = substr( $url, 3 );
-	                        array_pop( $cur_dir );
-	                    }
-	                }
-	            }
-	            elseif( substr( $matches[1][$i] , 0, 7 ) != 'http://' && substr( $matches[1][$i] , 0, 8 ) != 'https://' ) // do any links left without root
-	                $links[] = $url_info['scheme'] . '://' . $url_info['host'] . $url_info['path'] . $matches[1][$i];
-	            else
-	            {
+				if($matches[1][$i]{0} == '/') { // add host to any links
+					$links[] = $url_info['scheme'] . '://' . $url_info['host'] . $matches[1][$i];
+				} elseif($matches[1][$i]{0} == '.') {
+					$done = true;
+					$url = $matches[1][$i];
+					$cur_dir = explode('/', $url_info['path']);
+					array_shift($cur_dir);
+					array_pop($cur_dir);
+					
+					for($j = 0; isset($cur_dir[$j]); $j++) $cur_dir[$j] = '/' . $cur_dir[$j];
+					
+					while($done) {
+						/*  if no more ./ or ../ then it's done */
+						if($url{0} != '.') {
+							$links[] = $url_info['scheme'] . '://' . $url_info['host'] . implode('', $cur_dir) . '/' . $url;
+							$done = false;
+						} elseif(substr($url, 0, 2 ) == './') { /* remove same dir as that is the default */
+							$url = substr($url, 2);
+						} else {
+							$url = substr($url, 3);
+							array_pop($cur_dir);
+						}
+					}
+					
+            } elseif(substr($matches[1][$i] , 0, 7) != 'http://' && substr($matches[1][$i], 0, 8) != 'https://') { // do any links left without root
+					$links[] = $url_info['scheme'] . '://' . $url_info['host'] . $url_info['path'] . $matches[1][$i];
+            } else {
 					$links[] = $matches[1][$i];
-	            }
-	        }
-	    }
+				}	
+			}
+		}
 	
-	    return $links;
+		return $links;
 	}
 	
 	/**
 	 * Return HTML content from a URL.
 	 * 
-	 * CURL is used if available, falling back to parse_url()
-	 * if CURL is not installed with PHP.
-	 *
 	 * @param string $url The URL to get
 	 * @return mixed string contents or false
 	 */
@@ -205,44 +182,35 @@ class LinkCheckProcessor extends Object {
 		ini_set('user_agent', 'User-Agent: ' . self::$agent_name);
 		
 		if(($fp = fopen($url, 'r'))) {
-			for(;($data = fread($fp, 1024));) $contents .= $data;
+			while($data = fread($fp, 1024)) $contents .= $data;
+			
 			fclose($fp);
-		} elseif(function_exists('curl_init')) {
-			$ch = curl_init($url);
-			
-			$options = array(
-				CURLOPT_HEADER => false,
-				CURLOPT_USERAGENT => self::$agent_name,
-				CURLOPT_RETURNTRANSFER => true
-			);
-			
-			curl_setopt_array($ch, $options);
-
-			$contents .= curl_exec($ch);
-			
-			curl_close($ch);
-		} elseif(($url_info = parse_url($url))) {
+		} elseif($url_info = parse_url($url)) {
 			if($url_info['scheme'] == 'https') {
 				$fp = fsockopen('ssl://' . $url_info['host'], 443, $errno, $errstr, 30);
 			} else {
 				$fp = fsockopen($url_info['host'], 80, $errno, $errstr, 30);
 			}
 			
-			if(!$fp) return false;
-			
-			$out = 'HEAD ' . (isset($url_info['path']) ? $url_info['path'] : '/') .
-				(isset($url_info['query']) ? '?' . $url_info['query']: '') .
-				" HTTP/1.0\r\n";
-			
-			$out .= 'Host: ' . $url_info['host'] . "\r\n";
-			$out .= 'User-Agent: ' . self::$agent_name . "\r\n";
-			$out .= "Connection: Close\r\n\r\n";
-			
-			fwrite($fp, $out);
-			
-			for(;!feof($fp);) $contents .= fgets($fp, 128);
-			
-			list($headers, $content) = explode("\r\n\r\n", $contents, 2);
+			if(!$fp) {
+				echo "$errstr ($errno)\r\n";
+			} else {
+				$out = 'HEAD ' . (isset($url_info['path']) ? $url_info['path'] : '/') .
+					(isset($url_info['query']) ? '?' . $url_info['query']: '') .
+					" HTTP/1.1\r\n";
+				
+				$out .= 'Host: ' . $url_info['host'] . "\r\n";
+				$out .= 'User-Agent: ' . self::$agent_name . "\r\n";
+				$out .= "Connection: Close\r\n\r\n";
+				
+				fwrite($fp, $out);
+				
+				while(!feof($fp)) $contents .= fgets($fp, 128);
+				
+				fclose($fp);
+				
+				list($headers, $content) = explode("\r\n\r\n", $contents, 2);
+			}
 		}
 		
 		return $contents;
@@ -251,58 +219,41 @@ class LinkCheckProcessor extends Object {
 	/**
 	 * Return HTTP headers for a URL given.
 	 * 
-	 * CURL is used if available, falling back to parse_url()
-	 * if CURL is not installed with PHP.
-	 *
 	 * @param string $url The url to get the headers of
 	 * @return mixed string headers or false
 	 */
 	function fetchHeaders($url) {
 		$headers = '';
 
-		if(function_exists('curl_init')) {
-			$ch = curl_init($url);
-
-			$options = array(
-				CURLOPT_HEADER => false,
-				CURLOPT_NOBODY => true,
-				CURLOPT_FOLLOWLOCATION => true,
-				CURLOPT_USERAGENT => self::$agent_name,
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_ENCODING => ''
-			);
-			
-			curl_setopt_array($ch, $options);
-			curl_exec($ch);
-			
-			$headers = curl_getinfo($ch);
-			
-			curl_close($ch);
-		} elseif(($url_info = parse_url($url))) {
+		if($url_info = parse_url($url)) {
 			if($url_info['scheme'] == 'https') {
 				$fp = fsockopen('ssl://' . $url_info['host'], 443, $errno, $errstr, 30);
 			} else {
 				$fp = fsockopen($url_info['host'], 80, $errno, $errstr, 30);
 			}
 	
-			if(!$fp) return false;
-	
-			$out = 'HEAD ' . (isset($url_info['path']) ? $url_info['path'] : '/') .
-				(isset($url_info['query']) ? '?' . $url_info['query']: '') .
-				" HTTP/1.0\r\n";
+			if(!$fp) {
+				echo "$errstr ($errno)\r\n";
+			} else {
+				$out = 'HEAD ' . (isset($url_info['path']) ? $url_info['path'] : '/') .
+					(isset($url_info['query']) ? '?' . $url_info['query']: '') .
+					" HTTP/1.1\r\n";
+					
+				$out .= 'Host: ' . $url_info['host'] . "\r\n";
+				$out .= 'User-Agent: ' . self::$agent_name . "\r\n";
+				$out .= "Connection: Close\r\n\r\n";
+		
+				fwrite($fp, $out);
+
+				$contents = '';
+
+				while(!feof($fp)) $contents .= fgets($fp, 128);
 				
-			$out .= 'Host: ' . $url_info['host'] . "\r\n";
-			$out .= 'User-Agent: ' . self::$agent_name . "\r\n";
-			$out .= "Connection: Close\r\n\r\n";
-	
-			fwrite($fp, $out);
-			
-			$contents = '';
-			
-			for(;!feof($fp);) $contents .= fgets($fp, 128);
-	
-			list($headers, $content) = explode("\r\n\r\n", $contents, 2);
-			$headers = explode( "\r\n", $headers );
+				fclose($fp);
+		
+				list($headers, $content) = explode("\r\n\r\n", $contents, 2);
+				$headers = explode( "\r\n", $headers );
+			}
 		}
 		
 		return $headers;
@@ -311,13 +262,6 @@ class LinkCheckProcessor extends Object {
 	/**
 	 * Pull out the HTTP status code from the headers.
 	 * 
-	 * This is only needing to be used if CURL is not
-	 * installed.
-	 * 
-	 * @TODO Make sure this only returns a status code,
-	 * to make it consistent with the way the status code
-	 * is returned using CURL.
-	 *
 	 * @param array $headers List of headers
 	 * @return array
 	 */
