7e1133d24c14bba96be91b60bd586b9487c46460
sharvey <sharvey@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-10-14 01:09:26 +0000

    Added basic LinkCheckAdmin template variables
    git-svn-id: http://svn.silverstripe.com/open/modules/linkchecker/trunk@64177 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- code/LinkCheckAdmin.php
+++ code/LinkCheckAdmin.php
@@ -3,15 +3,31 @@
 /**
  * Link checker CMS interface.
  * 
+ * @uses LinkCheckRun to create a list of link check runs
+ * 
  * @package linkchecker
  */
 class LinkCheckAdmin extends LeftAndMain {
 	
+	/**
+	 * Return a link used to access this LinkCheckAdmin
+	 * interface in the CMS.
+	 *
+	 * @param string $action The action to call (defaults to index)
+	 * @return string
+	 */
 	public function Link($action = 'index') {
 		return "admin/linkcheck/$action/";
 	}
 	
+	/**
+	 * Return all instances of LinkCheckRun from
+	 * the database.
+	 *
+	 * @return DataObjectSet
+	 */
 	public function LinkCheckRuns() {
+		return DataObject::get('LinkCheckRun');
 	}
 	
 }
--- templates/Includes/LinkCheckAdmin_left.ss
+++ templates/Includes/LinkCheckAdmin_left.ss
@@ -1,1 +1,25 @@
-LEFT
+<style>
+	ul.tree a {
+		background-image: url(cms/images/treeicons/reports-file.png);
+	}
+</style>
+
+<h2><% _t('LINKCHECK','Link check runs') %></h2>
+
+<div id="treepanes">
+	<div id="sitetree_holder">
+		<% if LinkCheckRuns %>
+			<ul id="sitetree" class="tree unformatted">
+			<li id="$ID" class="root"><a><% _t('LINKCHECK','Link check runs') %></a>
+				<ul>
+				<% control LinkCheckRuns %>
+					<li id="$ID">
+						<a href="$BaseURL/admin/linkcheck/$ID">$Created.Nice</a>
+					</li>
+				<% end_control %>
+				</ul>
+			</li>
+			</ul>
+		<% end_if %>
+	</div>
+</div>
