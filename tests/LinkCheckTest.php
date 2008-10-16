15e2fc1079c813b3d528b2935ebade6b5e387ed7
sharvey <sharvey@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-10-16 01:07:54 +0000

    temporarily removed test
    git-svn-id: http://svn.silverstripe.com/open/modules/linkchecker/trunk@64336 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/LinkCheckTest.php
+++ tests/LinkCheckTest.php
@@ -4,8 +4,8 @@ class LinkCheckTest extends SapphireTest {
 	
 	function testLinkCheckerProcessorCreation() {
 		/* Start a new instance of LinkCheckProcessor with a URL that works */
-		$processor = new LinkCheckProcessor('http://www.silverstripe.com/community-overview');
-		Debug::show($processor->run());
+		//$processor = new LinkCheckProcessor('http://www.silverstripe.com/community-overview');
+		//Debug::show($processor->run());
 	}
 	
 }
