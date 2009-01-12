af0fcdb14659cf40bf44773e103b217bd0a5a861
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-01-12 05:03:37 +0000

    MINOR Updated language master tables
    git-svn-id: http://svn.silverstripe.com/open/modules/linkchecker/trunk@70010 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ lang/en_US.php
@@ -0,0 +1,41 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['LinkCheckAdmin']['MENUTITLE'] = array(
+	'LinkCheck',
+	100,
+	'Menu title'
+);
+$lang['en_US']['BrokenLink']['PLURALNAME'] = array(
+	'Broken Links',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['BrokenLink']['SINGULARNAME'] = array(
+	'Broken Link',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['LinkCheckAdmin']['CHECKRUN'] = 'Results';
+$lang['en_US']['LinkCheckAdmin']['FOLDERDELETED'] = 'link check run deleted';
+$lang['en_US']['LinkCheckAdmin']['FOLDERSDELETED'] = 'link check runs deleted';
+$lang['en_US']['LinkCheckAdmin_left.ss']['DELETE'] = 'Delete';
+$lang['en_US']['LinkCheckAdmin_left.ss']['DELFOLDERS'] = 'Delete the selected items';
+$lang['en_US']['LinkCheckAdmin_left.ss']['GO'] = 'Go';
+$lang['en_US']['LinkCheckAdmin_left.ss']['LINKCHECK'] = 'Link check runs';
+$lang['en_US']['LinkCheckAdmin_left.ss']['SELECTTODEL'] = 'Select the items that you want to delete and then click the button below';
+$lang['en_US']['LinkCheckAdmin_left.ss']['START'] = 'Start link checker';
+$lang['en_US']['LinkCheckAdmin_right.ss']['LINKCHECKHEADER'] = 'SilverStripe Link Checker';
+$lang['en_US']['LinkCheckRun']['PLURALNAME'] = array(
+	'Link Check Runs',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['LinkCheckRun']['SINGULARNAME'] = array(
+	'Link Check Run',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+
+?>
